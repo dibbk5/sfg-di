1159bdc32c5f74e6a369e67fa5ee20e9dcae7a08
Koy Dibb <dibbk5@gmail.com>
2023-05-15 11:15:40 -0600

    More Injection

--- src/test/java/com/example/sfgdi/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/com/example/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package com.example.sfgdi.controllers;
 
-import com.example.sfgdi.services.GreetingServiceImpl;
+import com.example.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyInjectedControllerTest {
 
@@ -14,7 +12,7 @@ class PropertyInjectedControllerTest {
     void setUp() {
         controller = new PropertyInjectedController();
 
-        controller.greetingService = new GreetingServiceImpl();
+        controller.greetingService = new ConstructorGreetingService();
     }
 
     @Test
